9ea5ac867c81b57f92d3fc0b40856e0f2fb40744
Volgin7 <8612020@gmail.com>
2023-05-17 02:23:56 +0300

    methods added

--- main/java/pro/sky/hwlistsspring/controller/EmployeeController.java
+++ main/java/pro/sky/hwlistsspring/controller/EmployeeController.java
@@ -10,13 +10,32 @@ import pro.sky.hwlistsspring.service.EmployeeService;
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
-
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+        this.employeeService.addEmployee("Alex", "Volgin");  // as example
+        this.employeeService.addEmployee("Ilya", "Sazonos");  // as example
+        this.employeeService.addEmployee("Artyem", "Petrosyan");  // as example
+        this.employeeService.addEmployee("Anastasiya", "Zabrodina");  // as example
+
     }
-@GetMapping("/add")
+
+    @GetMapping()
+    public String hello() {
+        return "Welcome to Employee Service";
+    }
+    @GetMapping("/add")
     public String add(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName ) {
-      return "New employee " + employeeService.addEmployee(firstName,lastName) + " added";
-}
+        return "New employee " + employeeService.addEmployee(firstName,lastName) + " added";
+    }
+
+    @GetMapping("/find")
+    public String find(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName ) {
+        return "Employee " + employeeService.findEmployee(firstName,lastName) + " found";
+    }
+
+    @GetMapping("/remove")
+    public String remove(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName ) {
+        return "Employee " + employeeService.removeEmployee(firstName,lastName) + " removed";
+    }
 
 }
--- main/java/pro/sky/hwlistsspring/service/EmployeeService.java
+++ main/java/pro/sky/hwlistsspring/service/EmployeeService.java
@@ -8,17 +8,43 @@ import java.util.List;
 
 @Service
 public class EmployeeService {
-    public final int MAX_NUMBER_OF_EMPLOYEES = 10;
+    private final int MAX_NUMBER_OF_EMPLOYEES = 5;
     private List<Employee> employees;
 
+
     public EmployeeService() {
-        this.employees = new ArrayList<>();
+        this.employees = new ArrayList<Employee>();
+    }
+    public int getMAX_NUMBER_OF_EMPLOYEES() {
+        return MAX_NUMBER_OF_EMPLOYEES;
     }
 
     public String addEmployee(String firstName, String lastName) {
         Employee newEmployee = new Employee(firstName,lastName);
         this.employees.add(newEmployee);
-        return this.employees.get(this.employees.size()).toString();
+        System.out.println("this.employees = " + this.employees);
+        return this.employees.get(this.employees.size() - 1).toString();
     }
+
+    public String removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName,lastName);
+        for (int i = 0; i < this.employees.size(); i++) {
+            if(this.employees.get(i).equals(employee)) {
+                this.employees.remove(i);
+                return employee.toString();
+            }
+        }
+        return "can't be";
+    }
+    public String findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName,lastName);
+        for (int i = 0; i < this.employees.size(); i++) {
+            if(this.employees.get(i).equals(employee)) {
+                return this.employees.get(i).toString();
+            }
+        }
+        return "can't be";
+    }
+
 
 }
